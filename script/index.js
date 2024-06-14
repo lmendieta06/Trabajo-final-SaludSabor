1e3e59803ce24ddf100173e5a17d74d23c03ea4c
Laura Mendieta <laumendieta06@gmail.com>
2024-06-13 21:43:36 -0500

    [UPDATE] MODALES FUNCIONANDO

--- script/index.js
+++ script/index.js
@@ -1,8 +1,6 @@
 // ABRIR EL MODAL DE CONSTRUCCION
 // Almacenar la clase relacionada con el boton el en HTML
-const openModalConstruction = document.getElementsByClassName("recetas");
-
-
+const openModalConstruction = document.getElementsByClassName("enProceso");
 // Para que el modal aparezca lo guardo en otra variable
 const modalConstruction = document.querySelector(".modalConstruccion");
 
@@ -11,35 +9,32 @@ const closeModalConstrucction = document.querySelector(".modalConstruccionClose"
 
 // DEFINIR QUE DEBE PASAR PARA ABRIR EL MODAL CONSTRUCCION
 // Definir en que tipo de evento quiero que pase
-openModalConstruction.addEventListener("click", (e)=>{
-    // Evitar que se muestre la almohadilla predefinida en el a
+Array.from(openModalConstruction).forEach(element => {
+    element.addEventListener("click", (e)=>{
+        e.preventDefault();
+        // Mostrar el modal construccion
+        // Agrega la funcion modal--show definida en el css
+        modalConstruction.classList.add("modalConstruccion--show");
+    })
+});
+closeModalConstrucction.addEventListener("click", (e) => {
     e.preventDefault();
-    // Mostrar el modal construccion
-    // Agrega la funcion modal--show definida en el css
-    modalConstruction.classList.add("modalConstruccion--show");
-})
-
-// DEFINIR QUE DEBE PASAR PARA CERRAR EL MODAL CONSTRUCCION
-closeModalConstrucction.addEventListener("click", (e)=>{
-    // Evitar que se muestre la almohadilla predefinida en el a
-    e.preventDefault();
-    // Mostrar el modal construccion
-    // Agrega la funcion modal--show definida en el css
     modalConstruction.classList.remove("modalConstruccion--show");
-})
+});
 
 // LOGICA MODAL INICIAR SESION
-const openModal = document.getElementsByClassName(".boton-clave");
+const openModal = document.getElementsByClassName("boton-clave");
 const modal = document.querySelector(".modalInicioSesion");
 const closeModal = document.querySelector(".Cerrar-modal");
 
-openModal.addEventListener("click", (e)=>{
-    e.preventDefault();
-    modal.classList.add("modal--mostrar");
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.classList.add("modal--mostrar");
+    });
+}
 
-});
-closeModal.addEventListener("click", (e)=>{
+closeModal.addEventListener("click", (e) => {
     e.preventDefault();
     modal.classList.remove("modal--mostrar");
-
 });
